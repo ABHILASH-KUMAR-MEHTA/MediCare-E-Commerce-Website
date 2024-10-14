167f5d156d3535911992c85020c4457b8e174944
ABHILASH-KUMAR-MEHTA <145607994+ABHILASH-KUMAR-MEHTA@users.noreply.github.com>
2024-10-14 15:13:05 +0530

    first commit

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';  // or const jwt = require('jsonwebtoken');
+
+import userModel from '../models/userModel.js';
+
+// protected Routes token base
+export const requireSignIn = async (req, res, next) => {
+    const token = req.header("authentication");
+    if (!token) {
+        return res.status(401).send({ error: "Please authenticate using a valid token" });
+    }
+
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = data;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: "Please authenticate using a valid token" });
+    }
+}
+
+export const isAdmin = async (req, res, next) => {
+    try {
+        const user = await userModel.findById(req.user._id);
+        if (user.role !== 1) {
+            return res.status(401).send({
+                success: false,
+                message: "UnAuthorized Access"
+            });
+        } else {
+            next();
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "something went wrong"
+        })
+    }
+}
